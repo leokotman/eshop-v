1a1cd4cc32b1d26ca94d86aab81ff9ce374b2bd5
levkotman <leoktmn@yandex.ru>
2021-04-17 18:50:14 +0200

    created header components

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="menu-links">
+        <span><a href="#">Home</a></span>
+        <span><a href="#">Products</a></span>
+        <span><a href="#">About us</a></span>
+        <span><a href="#">Contacts</a></span>        
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style scoped>
+.menu-links{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="search">
+		<input type="text" class="goods-search" />
+		<button class="search-button" type="button" v-on:click="filter" >Search</button>
+	</div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    props: {
+        goods: Array,
+    },
+    methods: {
+        filter(){
+            console.log('filter worked');
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
