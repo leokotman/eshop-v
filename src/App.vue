c4c71e87e09ef7161490e730fa2c16826b56c304
levkotman <leoktmn@yandex.ru>
2021-05-03 03:01:17 +0200

    added opened cart from cart button click

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,40 @@
 <template>
   <div id="app">
-    <Header :goods='goods' @header-search="filteredContent"/>
+    <Header :goods='goods' @header-search="filterContent" @open-cart="openCart" />
+    <Cart v-show="cartOpened"/>
     <ul v-if="noSearchWorked">
-      <li v-for="good in goods" v-bind:key="good.id_product"> {{good.product_name}} {{good.price}}</li>
+      <li v-for="good in goods" v-bind:key="good.id_product">
+         {{ good.product_name}}
+         {{good.price}}</li>
     </ul>
     <ul v-else>
-      <li v-for="good in filteredGoods" v-bind:key="good.id_product"> {{ good.product_name }} {{good.price}} </li>
+      <li v-for="good in filteredGoods" v-bind:key="good.id_product">
+        {{ good.product_name }}
+        {{good.price}} </li>
     </ul>
-
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Cart from './components/Cart';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Cart,
   },
   data: function(){
     return {
       goods: [],
       filteredGoods: [],
       noSearchWorked: true,
+      cartOpened: false,
     };
   },
   methods: {
-    filteredContent(searchText){
+    filterContent(searchText){
       console.log(searchText);
       let searchedGoods = this.goods.filter(good => {
         return good.product_name.includes(searchText);
@@ -40,6 +47,13 @@ export default {
         .then(response => response.json())
         .then(data => (this.goods = data));
     },
+    openCart() {
+      if (this.cartOpened){
+        return this.cartOpened = false;
+      } else {
+        return this.cartOpened = true;
+      }
+    }
   },
   mounted () {
     this.fetchGoods();
---
+++ src/components/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h2>Cart</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero magni laborum ratione minus quidem facere animi dolorem numquam, itaque quam sed aliquam eveniet expedita sit similique ea. Sed, fuga sapiente?</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Cart",
+
+}
+</script>
+
+<style scoped>
+
+</style>
